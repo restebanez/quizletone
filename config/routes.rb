e0e3f7d101167e8fe836e1696ab99564b215ccbe
Rodrigo Estebanez <rodhom@gmail.com>
2017-09-20 12:33:34 +0100

    sets controller scheleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #get 'sets/index'
+
+  #get 'sets/show'
+  resources :sets, only: [:index, :show]
+
+  root 'sets#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
