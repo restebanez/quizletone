6bcc27b9881307c46e6e644468dd2f6dcda54b48
Rodrigo Estebanez <rodhom@gmail.com>
2017-09-20 13:38:29 +0100

    refactor out the extraction so it's testable

--- app/models/quizlet_set.rb
+++ app/models/quizlet_set.rb
@@ -11,8 +11,12 @@ class QuizletSet
   USERS_URL = "#{BASE_URL}/#{USERNAME}"
 
   def self.list
-    response_body = RestClient.get(USERS_URL, {params: {client_id: CLIENT_ID}}).body
-    YAML.load(response_body)["sets"].map do |set|
+    response_body_str = RestClient.get(USERS_URL, {params: {client_id: CLIENT_ID}}).body
+    self.new.extract_attrs(YAML.load(response_body_str))
+  end
+  
+  def extract_attrs(response_body_hash)
+    response_body_hash["sets"].map do |set|
       set.slice("id", "url", "title")
     end
   end
